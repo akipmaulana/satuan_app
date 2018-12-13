a4e22222022b3b9947027bc66c1f3689c473db89
Akip Maulana <akip.maulana@gmail.com>
2018-12-13 14:21:55 +0700

    Add InkWell in Home Menu

--- lib/src/ui/widgets/category_rectangle.dart
+++ lib/src/ui/widgets/category_rectangle.dart
@@ -1,40 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:satuan_app/src/models/category.dart';
+import 'package:satuan_app/src/ui/themes/default_res.dart';
 
 class CategoryContainer extends StatelessWidget {
-
   final Category category;
 
   CategoryContainer(this.category);
 
   @override
   Widget build(BuildContext context) {
-    return new Container(
-      decoration: new BoxDecoration(
-        borderRadius: BorderRadius.circular(4.0),
-        color: category.color,
-      ),
-      margin: new EdgeInsets.all(1.0),
-      child: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Image.asset(
-              category.imageLocation,
-              width: 32.0,
-              height: 32.0,
-            ),
-            new Text(
-              category.title,
-              style: new TextStyle(
-                fontSize: 14,
-                color: Colors.white,
-              ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
+    return new InkWell(
+        highlightColor: category.color,
+        splashColor: category.color,
+        onTap: () {
 
+        },
+        child: new Container(
+          decoration: new BoxDecoration(
+            borderRadius: BorderRadius.circular(4.0),
+            color: category.color,
+          ),
+          margin: new EdgeInsets.all(2.0),
+          child: new Center(
+            child: new Padding(
+              padding: EdgeInsets.all(DefaultDimen.spaceLarge),
+              child: new Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  new Image.asset(
+                    category.imageLocation,
+                    width: 32.0,
+                    height: 32.0,
+                  ),
+                  new Text(
+                    category.title,
+                    style: new TextStyle(
+                      fontSize: DefaultDimen.textMedium,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
 }
