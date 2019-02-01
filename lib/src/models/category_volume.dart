5110a6ccd1b5153f8f29c77bf6261d8f889cfa0a
Akip Maulana <akip.maulana@gmail.com>
2019-02-01 22:41:24 +0700

    volume calculator

--- lib/src/models/category_volume.dart
+++ lib/src/models/category_volume.dart
@@ -1,9 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'category.dart';
 import 'unit.dart';
 import 'package:satuan_app/src/ui/themes/default_res.dart';
 
-class CategoryVolume implements Category {
+class CategoryVolume extends Category {
   @override
   Color color = DefaultColor.red;
 
@@ -22,57 +24,57 @@ class CategoryVolume implements Category {
   @override
   List<Unit> units = [
     Unit(
-      index: 0,
+      index: 1,
       title: 'Kilometer Kubik',
       abbreviation: 'km3',
       value: 0.0,
     ),
     Unit(
-      index: 1,
+      index: 2,
       title: 'Hektometer Kubik',
       abbreviation: 'hm3',
       value: 0.0,
     ),
     Unit(
-      index: 2,
+      index: 3,
       title: 'Dekameter Kubik',
       abbreviation: 'dam3',
       value: 0.0,
     ),
     Unit(
-      index: 3,
+      index: 4,
       title: 'Meter Kubik',
       abbreviation: 'm3',
       value: 0.0,
     ),
     Unit(
-      index: 4,
+      index: 5,
       title: 'Desimeter Kubik',
       abbreviation: 'dm3',
       value: 0.0,
     ),
     Unit(
-      index: 5,
+      index: 6,
       title: 'Sentimeter Kubik',
       abbreviation: 'cm3',
       value: 0.0,
     ),
     Unit(
-      index: 6,
+      index: 7,
       title: 'Milimeter Kubik',
       abbreviation: 'mm3',
       value: 0.0,
     ),
   ];
 
-  @override
-  List<Unit> calculate({@required String from, @required double value}) {
-    return units;
-  }
-
-  @override
-  double calculator(double value, {String from, String to}) {
-    // TODO: implement calculator
-    return null;
+  double calculator(double value,
+      {@required String from, @required String to}) {
+    if (from == to) {
+      return value;
+    }
+    final startIndex = units.firstWhere((unit) => unit.title == from).index;
+    final endIndex = units.firstWhere((unit) => unit.title == to).index;
+    final exponent = endIndex - startIndex;
+    return value * pow(1000, exponent);
   }
 }
